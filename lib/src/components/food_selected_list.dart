09d8f2ddc9000455bdca948b7ba981b62a9a80ca
Breno <brenomenosso200@gmail.com>
2024-05-23 13:23:49 -0300

    adicionado componente que foi esquecido no commit anterior

---
+++ lib/src/components/food_selected_list.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:forkify_app/src/components/card_modal_options.dart';
+import 'package:forkify_app/src/helpers/messages.dart';
+import 'package:forkify_app/src/utils/food_types.dart';
+
+class FoodSelectedList extends StatefulWidget {
+  const FoodSelectedList({super.key});
+
+  @override
+  State<FoodSelectedList> createState() => _FoodSelectedListState();
+}
+
+class _FoodSelectedListState extends State<FoodSelectedList> with MessageViewMixin {
+
+  void _closeModal(BuildContext context, String food) {
+    Navigator.pop(context, food);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title:
+            Text('Pratos', style: Theme.of(context).textTheme.headlineMedium),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                  ListView.builder(
+                    itemCount: FoodTypes.types.length,
+                    itemBuilder: (context, index) {
+                      final food = FoodTypes.types[index];
+                      return CardModalOption(
+                        option: food,
+                        onTap: () {
+                          _closeModal(context, food);
+                        },
+                      );
+                    },
+                  )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
